77ecc818aafee5b0fcc212480a9960bfaf388351
darshanparmar18 <darshanparmar123416@gmail.com>
2024-10-26 15:17:14 +0530

    changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { CartComponent } from '../cart/cart.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CartComponent],
+  imports: [CartComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
